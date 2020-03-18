e8eb1ac0920f789b281b12851e656822b2ed5c80
Andrew O <61236243+AndyOrv@users.noreply.github.com>
2020-03-18 16:12:39 +0000

    New inventory system
    -added vector<item> to player and reworked Item class

--- FourShield/main.cpp
+++ FourShield/main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
+#include <vector>
 using namespace std;
 
     /***************************************Objects and Characters*********************/
 //Player class
+
+
+class Item{ // basic item characteristics
+    public:
+        int damage;
+        string name;
+
+        Item(string nam, int dmg){
+            damage = dmg;
+            name = nam;
+
+        }
+
+};
+
 class Player{
     public:
         char name[50];
 
+        vector<Item> inv;
+
+        void printInv(){
+         cout << "\nINVENTORY \n";
+           for (int x = 0; x <= inv.size(); x++)
+            cout<< "  " << inv[x].name << endl;
+        }
+
     };
 
-class Item{ // basic item characteristics
-    public:
-        bool got = false;
-
-        void get(){
-        got = true;
-        }
-};
-
-
-class Gun : public Item{
-
-};
 
 
 
@@ -39,13 +50,20 @@ void contin(){ // End of Page Message
     _getch();
 }
 
-int choice;
 
-void c11(){
+
+/***********INITIALIZE**************/
+int choice;
+Player player;
+
+
+//CHOICE 2.1
+void c21(){
 
     system("cls");
     header("Outside");
-        Gun handgun;
+        Item gun("Handgun", 50);//initialize handgun
+        Item flower("Rose", 0);//initialize flower
         cout << "You leave the room and find yourself in a deep forest, in front of you is a box on the box lies a handgun and a flower" << endl;
         cout << "\n\t |1|> Take the Gun" << endl;
         cout << "\t |2|> Take the Flower" << endl;
@@ -57,13 +75,16 @@ void c11(){
 
             case 1:
                 cout << "\nYou take the gun" << endl;
-                handgun.get();
-                cout<< handgun.got << endl;
+                player.inv.push_back(gun);
+                player.inv.push_back(flower);
+                //cout << player.inv.size()<<endl;
+                player.printInv();
                 contin();
                 break;
 
             case 2:
                 cout << "\nYou take the flower" << endl;
+                player.inv.push_back(flower);
                 contin();
                 break;
 
@@ -77,7 +98,6 @@ void c11(){
 
 int main()
 {
-    Player player; //create player
     header("Darkness");
     cout << "You sit in a dark room a man stand in front of you" << endl;
     cout << "\n\t Voice: Your name, what is it" << endl;
@@ -101,7 +121,7 @@ int main()
             cout << "\t person: shutup." << endl;
             cout << "You exit the room." << endl;
             contin();
-            c11();
+            c21();
         break;
 
         case 2:
